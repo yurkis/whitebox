fd717e29f26ca0eca8d5571b358a078297f56a83
yurkis <yurkis@gmail.com>
2015-01-05 18:38:03 +0200

    Dev update from git

--- sysmanager/modules/comms/dev/dev.py
+++ sysmanager/modules/comms/dev/dev.py
@@ -4,10 +4,11 @@ from ...result import *
 ################################################################################
 def do_dev_update(args):
   #print("\n\nUpdating sysmanager...")
-  out_sysman=subprocess.getoutput("svn up /usr/local/bin/pwb")
+  ##out_sysman=subprocess.getoutput("svn up /usr/local/bin/pwb")
   #print (out_sysman)
   
   #print ("\n\nUpdating html...")
-  out_html=subprocess.getoutput("svn up /usr/local/www/pwb/html")
-  return (Result(out_html + "\n\n" + out_sysman))
+  ##out_html=subprocess.getoutput("svn up /usr/local/www/pwb/html")
+  out_html=subprocess.getoutput("cd /root/whitebox && git pull")
+  return (Result(out_html))
 
