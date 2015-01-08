a48bff6c58edfe514d2b2f88850262fa186b8934
yurkis <yurkis@gmail.com>
2015-01-08 13:52:51 +0200

    first commit with twitter bootstrap

---
+++ html/settings.php
@@ -0,0 +1,47 @@
+<!--<html>
+<body>
+<h1> It works!</h1>
+<h3><a href="update.php">Update!!</a></h3>
+<h3><a href="sample_hwinfo.php">Sample sys info (JSON test)</a></h3>
+<h3><a href="sample_cpuload.php">Sample CPU load info (JSON test)</a></h3>
+<h3><a href="list_sys_commands.php">Sys commands list</a></h3>
+<?php
+phpinfo();
+?>
+</body>
+</html>
+
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bootstrap 101 Шаблон</title>
+    <!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+
+    <div class="navbar">
+    <div class="navbar navbar-static-top">
+    <a class="brand" href="#">Whitebox</a>
+    <ul class="nav">
+    <li><a href="index.php">Home</a></li>
+    <li class="active"><a href="#">Setings</a></li>
+    <li><a href="#">Devel</a></li>
+    <li><a href="#">Exit</a></li>
+    </ul>
+    </div>
+    </div>
+
+
+
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
